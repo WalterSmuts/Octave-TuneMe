4ddf91c326fcddbc22df5275d349b9639cb5d96b
Walter Smuts <smuts.walter@gmail.com>
2018-08-16 21:25:09 +0200

    Added vocoder library and a testing script

---
+++ Vocoder Tests/TestVariableTimeScaling.m
@@ -0,0 +1,26 @@
+pkg load signal;
+addpath("lib/");
+
+% Variable initialization
+[x sf] = audioread("testData.wav");
+windowSize = 1024;
+hop = windowSize/4;
+ratio = 2;
+
+% Convert to stft
+spec = stft(x',windowSize,windowSize,hop);
+original = columns(spec)-2;
+
+% Sample at variable intervals
+t = 1:original;
+t = (t.^2)./original;
+
+% Sample at constant intervals
+%t = 0:ratio:original
+
+% Actually apply re-sampling
+newSpec = pvsample(spec,t,hop);
+newX = istft(newSpec,windowSize,windowSize,hop);
+
+% Play Slowed Down sample
+soundsc(newX,sf);
