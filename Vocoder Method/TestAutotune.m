9bdd37c56047a75c022eb80b2929b8916f980c4f
Walter Smuts <smuts.walter@gmail.com>
2018-08-19 17:47:26 +0200

    Setup testing environment
    For sampling points from shifting ratio

--- Vocoder Method/TestAutotune.m
+++ Vocoder Method/TestAutotune.m
@@ -4,7 +4,7 @@ addpath("lib/");
 % Variable initialization
 sf = 44100;
 t = 0:1/sf:2;
-x = sin(2*pi*440*t );
+x = sin(2*pi*440*t + 440*t.^2);
 windowSize = 1024;
 hop = windowSize/4;
 ratio = 2;
@@ -16,6 +16,7 @@ spec = stft(x',windowSize,windowSize,hop);
 freqContour = getFrequencyContourSTFT(spec,sf,windowSize);
 wantedContour = getClosestFreqContour(freqContour);
 shiftRatio = wantedContour./freqContour;
+shiftRatio = ones(1,length(shiftRatio))*0.8;
 
 % Sample at arbitrary variable intervals
 samplePoints = 1:columns(spec)-1; % -1 because needs n+1 to interpolate
@@ -23,13 +24,21 @@ samplePoints = samplePoints.^2/(samplePoints(end));
 
 % Actually apply arbitrary re-sampling
 newSpec = pvsample(spec,samplePoints,hop);
-strechedSpec = istft(newSpec,windowSize,windowSize,hop);
+stretchedX = istft(newSpec,windowSize,windowSize,hop);
 
 % Interpolate to pitch shifted sample by arbitrary function
-tCompress = 1:length(strechedSpec);
+tCompress = 1:length(stretchedX);
 tCompress = sqrt(tCompress);
 tCompress = tCompress .* tCompress(end);
-autoTunedX = interp1(strechedSpec, [tCompress], 'spline');
+autoTunedX = interp1(stretchedX, [tCompress], 'spline');
+
+% Draw sampling density function
+subplot(2,1,1);
+stem(shiftRatio);
+% Draw Sample point
+subplot(2,1,2);
+scatter(samplePoints,zeros(1,length(samplePoints)))
 
 % Play
+soundsc(x,sf);
 soundsc(autoTunedX,sf);
