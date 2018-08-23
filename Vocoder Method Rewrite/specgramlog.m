4ce62cef8d074e7eee82f64c75324d1eb354e000
Walter Smuts <smuts.walter@gmail.com>
2018-08-23 15:14:12 +0200

    Commented warning in specgramlog
    May display inconsistently for newer versions of octave
    or on Matlab

--- Vocoder Method Rewrite/specgramlog.m
+++ Vocoder Method Rewrite/specgramlog.m
@@ -1,4 +1,6 @@
 function specgramlog(signal,segmentSize,sf)
+	% Running this causes a Warning!!!
+	% Might not be consistent over implementations
 	% Get spectrogram
 	[spec frequency time] = specgram(signal,segmentSize,sf);
 	spec = 20*log10(abs(spec));
