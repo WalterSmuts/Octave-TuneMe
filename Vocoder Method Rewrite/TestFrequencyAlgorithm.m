e7e1a294cfdb78402d4a407f9695a0a89402cf37
Walter Smuts <smuts.walter@gmail.com>
2018-08-27 14:24:30 +0200

    Updated ZCM and added a way to inspect methods

---
+++ Vocoder Method Rewrite/TestFrequencyAlgorithm.m
@@ -0,0 +1,38 @@
+% Define constants
+filename = input("Please enter the name of a file to test with: ","s")
+if (isempty(filename))
+	filename = "singing.wav";
+endif
+[originalSample sf] = audioread(filename);
+segmentSize = 2048;
+con = zeros(length(originalSample)/segmentSize,1);
+
+% Add zeros to complete final segment
+originalSample = addZeros(originalSample,segmentSize);
+
+% Pre-filter signal for ZCM: Assume: 20 < signal < 250Hz
+[b,a] = butter(8, 250/sf*2);
+filteredSample = filter(b,a,originalSample);
+
+% Segment sample
+segO = segment(originalSample,segmentSize);
+segF = segment(filteredSample,segmentSize);
+
+% Iterate over segment
+for (i = 1:columns(seg))
+	% Calculate frequency of segment
+	s = segF(:,i);
+	con(i) = getFrequencyZCM(s,0,sf);
+	con(i)
+
+	% Plot segment
+	subplot(2,1,1);
+	plot(s);
+	grid;
+
+	% Plot frequency contour
+	subplot(2,1,2);
+	stem(con);
+
+	pause;
+endfor
--- Vocoder Method Rewrite/getFrequencyZCM.m
+++ Vocoder Method Rewrite/getFrequencyZCM.m
@@ -1,15 +1,23 @@
 function freq = getFrequencyZCM(signal, prevFreq,sf)
-	% Calculate crossing points
-	zc = zerocrossing(1:length(signal),signal);
-	firstIndex = zc(1);
-	lastIndex  = zc(end);
-
-	% Calculate frequency
-	sum = length(zc)-1;
-	freq = sum/2*(sf/(lastIndex-firstIndex));
-
 	% Somewhat a voice presence check
 	if (max(signal) < 0.02);
 		freq = prevFreq;
+		return;
 	endif
+
+	% Calculate crossing points
+	zc = zerocrossing(1:length(signal),signal);
+
+	% If no crossings
+	if (length(zc) == 0)
+		freq = prevFreq;
+		return;
+	endif
+
+	% Get considered length
+	consideredLength = zc(end) - zc(1);
+
+	% Calculate frequency
+	sum = length(zc)-1;
+	freq = sum/2*(sf/consideredLength);
 endfunction
