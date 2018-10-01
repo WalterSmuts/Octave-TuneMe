ae78dbcc06719d1aea477f1cb32fb3187d3db845
Walter Smuts <smuts.walter@gmail.com>
2018-10-01 12:46:37 +0200

    Improved harmonic figure

--- Thesis-Figures/Harmony.m
+++ Thesis-Figures/Harmony.m
@@ -1,28 +1,32 @@
 t = 0:1/100:3;
 x1 = sin(2*pi*t*1);
 x2 = sin(2*pi*t*2);
-x  = x1 .+ x2;
+x  = (x1 .+ x2)/2;
 
 y1 = sin(2*pi*t*2);
 y2 = sin(2*pi*t*3);
-y  = y1 .+ y2;
+y  = (y1 .+ y2)/2;
 
 plot(t,x1,"linewidth", 2);
 hold on
 plot(t,x2,"linewidth", 2);
-hold off;
+hold off
 title("Individual");
 xlabel("Time");
 ylabel("Amplitude");
-set(gca,'XTick', [], 'YTick', [], 'fontsize', 25)
+set(gca,'XTick', [], 'YTick', [], 'fontsize', 30)
 set(gcf,'PaperUnits','inches','PaperPosition',[0 0 15 5])
 print -dpng 'HarmonyOctaveSeparate'
 
 plot(t,x,"linewidth", 2);
+hold on;
+plot([1,1],[-1,1],"color", 'k',"linestyle",'--');
+plot([2,2],[-1,1],"color", 'k',"linestyle",'--');
+hold off;
 title("Superposition");
 xlabel("Time");
 ylabel("Amplitude");
-set(gca,'XTick', [], 'YTick', [], 'fontsize', 25)
+set(gca,'XTick', [], 'YTick', [], 'fontsize', 30)
 set(gcf,'PaperUnits','inches','PaperPosition',[0 0 15 5])
 print -dpng 'HarmonyOctaveSuper.png'
 
@@ -33,14 +37,18 @@ hold off;
 title("Individual");
 xlabel("Time");
 ylabel("Amplitude");
-set(gca,'XTick', [], 'YTick', [], 'fontsize', 25)
+set(gca,'XTick', [], 'YTick', [], 'fontsize',30)
 set(gcf,'PaperUnits','inches','PaperPosition',[0 0 15 5])
 print -dpng 'HarmonyFifthSeparate'
 
 plot(t,y,"linewidth", 2);
+hold on;
+plot([1,1],[-1,1],"color", 'k',"linestyle",'--');
+plot([2,2],[-1,1],"color", 'k',"linestyle",'--');
+hold off;
 title("Superposition");
 xlabel("Time");
 ylabel("Amplitude");
-set(gca,'XTick', [], 'YTick', [], 'fontsize', 25)
+set(gca,'XTick', [], 'YTick', [], 'fontsize', 30)
 set(gcf,'PaperUnits','inches','PaperPosition',[0 0 15 5])
 print -dpng 'HarmonyFifthSuper.png'
