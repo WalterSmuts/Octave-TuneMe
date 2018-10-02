b49e94d496e1f57886125251479e436e24b0334e
Walter Smuts <smuts.walter@gmail.com>
2018-10-02 15:23:00 +0200

    Start from 440 Hz and lower case letters

--- Thesis-Figures/FrequencyVsPitch.m
+++ Thesis-Figures/FrequencyVsPitch.m
@@ -1,9 +1,8 @@
-A1 = 55;
 multiplier = 1.0594631;
 noteNumber = 24;
-notes = 55*multiplier.^(0:noteNumber)
+notes = 440*multiplier.^(0:noteNumber)
 stem(0:(length(notes)-1),notes);
-xlabel("Pitch (LOW to HIGH)");
+xlabel("Pitch (low to high)");
 ylabel("Frequency");
 set(gca,'XTick',0:2:noteNumber, 'fontsize', 20)
 set(gca, 'xtickLabel', {
