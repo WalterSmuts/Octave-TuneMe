375e02a86e1542e8f950f8d4505004bdc3398aac
Walter Smuts <smuts.walter@gmail.com>
2018-10-14 21:16:13 +0200

    Hanning window jou fokken toebie!!!

--- Overlap-And-Add-Method/getSegmentedSample.m
+++ Overlap-And-Add-Method/getSegmentedSample.m
@@ -2,7 +2,7 @@ function segmentedSample = getSegmentedSample(original,windowStartPoints,segment
 	segmentedSample = [];
 	windowStartPoints(1) = 1;
 	for (i = 1:length(windowStartPoints))
-		next = original(windowStartPoints(i):(windowStartPoints(i) + segmentSize-1));
+		next = original(windowStartPoints(i):(windowStartPoints(i) + segmentSize-1)).*hann(segmentSize);
 		segmentedSample = [segmentedSample next];
 	endfor
 endfunction
