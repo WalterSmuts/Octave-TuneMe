2ea9402aef9fef678b8f9de80cc8aa81e0890c39
Walter Smuts <smuts.walter@gmail.com>
2018-10-20 21:07:53 +0200

    Commented code

--- Overlap-And-Add-Method/getSegmentedSample.m
+++ Overlap-And-Add-Method/getSegmentedSample.m
@@ -3,7 +3,7 @@ function segmentedSample = getSegmentedSample(original,windowStartPoints,segment
 	windowStartPoints(1) = 1;
 	for (i = 1:length(windowStartPoints))
 		start = windowStartPoints(i);
-		stop  = windowStartPoints(i) + segmentSize-1);
+		stop  = windowStartPoints(i) + segmentSize-1;
 		next  = original(start:stop).*hann(segmentSize);
 		segmentedSample = [segmentedSample next];
 	endfor
--- Overlap-And-Add-Method/overlapAndAdd.m
+++ Overlap-And-Add-Method/overlapAndAdd.m
@@ -1,9 +1,12 @@
 function stretchedX = overlapAndAdd(seg)
+	% Constants
 	windowSize = rows(seg);
 	hopSize = windowSize/2;
 
-	%% Overlap and add
+	% Pre-allocate array
 	stretchedX = zeros((rows(seg)*columns(seg)+rows(seg))/2,1);
+
+	% Overlap and add
 	begin = 1;
 	for (i = 1:(columns(seg)))
 		last = begin + windowSize-1;
