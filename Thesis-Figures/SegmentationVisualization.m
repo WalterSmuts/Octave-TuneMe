3335c82d428416bd6721a258116d236ba808d66d
Walter Smuts <smuts.walter@gmail.com>
2018-10-21 11:32:48 +0200

    Aligned y-axis on segmentation demo

--- Thesis-Figures/SegmentationVisualization.m
+++ Thesis-Figures/SegmentationVisualization.m
@@ -15,28 +15,28 @@ plot(x);
 title("Original");
 xlabel("Sample Number");
 ylabel("Amplitude");
-axis([1 length(x)]);
+axis([1 length(x) -0.801 0.801]);
 grid;
 
 subplot(2,3,4);
 plot(seg(:,1));
 title("Segment 1");
 ylabel("Amplitude");
-axis([1 length(seg(:,1))]);
+axis([1 length(seg(:,1)) -0.801 0.801]);
 grid;
 
 subplot(2,3,5);
 plot(2048/2:(2048*1.5-1),seg(:,2));
 title("Segment 2");
 ylabel("Amplitude");
-axis([2048/2 (2048*1.5-1)]);
+axis([2048/2 (2048*1.5-1) -0.801 0.801]);
 grid;
 
 subplot(2,3,6);
 plot(2048:(2048*2-1),seg(:,3));
 title("Segment 3");
 ylabel("Amplitude");
-axis([2048 (2048*2-1)]);
+axis([2048 (2048*2-1) -0.801 0.801]);
 grid;
 
 print -dpng 'SegmentationVisualization.png'
