941af68ad32aed448dbf25887d30eee585cb2182
Walter Smuts <smuts.walter@gmail.com>
2018-10-21 12:34:40 +0200

    Fixed figure font size etc...

--- Thesis-Figures/ClipVsNoDemo.m
+++ Thesis-Figures/ClipVsNoDemo.m
@@ -1,6 +1,5 @@
 addpath("../AutoCorr/");
-set(gcf,'PaperUnits','inches','PaperPosition',[0 0 12 6])
-set(gca,'fontsize', 12)
+set(gcf,'PaperUnits','inches','PaperPosition',[0 0 30 15])
 
 [x sf] = audioread("singing.wav");
 start  = 1024;
@@ -14,29 +13,31 @@ xAutoCorr = xcorr(x);
 xAutoCorr = xAutoCorr(ceil(length(xAutoCorr)/2):end);
 xClipAutoCorr = xcorr(xclip);
 xClipAutoCorr = xClipAutoCorr(ceil(length(xClipAutoCorr)/2):end);
-size(t)
-size(xClipAutoCorr)
 
 subplot(2,2,1);
 plot(t,x,"linewidth", 2);
+set(gca,'fontsize', 25)
 title("Original Signal");
 xlabel("Time (s)")
 grid;
 
 subplot(2,2,2);
 plot(t,xclip,"linewidth", 2);
+set(gca,'fontsize', 25)
 title("Clipped Signal");
 xlabel("Time (s)")
 grid;
 
 subplot(2,2,3);
 plot(t, xAutoCorr,"linewidth", 2);
+set(gca,'fontsize', 25)
 title("Original Signal forward Autocorrelation");
 xlabel("Delay (s)")
 grid;
 
 subplot(2,2,4);
 plot(t, xClipAutoCorr,"linewidth", 2);
+set(gca,'fontsize', 25)
 title("Clipped Signal forward Autocorrelation");
 xlabel("Delay (s)")
 grid;
