25487916ee56004ee77bf398fce5fcc4af7c3e0c
Walter Smuts <smuts.walter@gmail.com>
2018-10-21 15:06:44 +0200

    Created metric for noise robustness

---
+++ MetricTests/TestNoiseRobustnessZCM.m
@@ -0,0 +1,32 @@
+pkg load image;
+sf = 44100;
+t = 0:1/sf:5;
+
+k = (2)^(1/10);
+x = sin(2*pi*110*(k.^t-1)/log(k));
+c = getFrequencyContour(x,2048,1024,sf);
+c(1) = 110;
+d = c;
+groundTruth = log2(c);
+
+for (i = 1:200)
+	noise = i/100*rand(1,length(x));
+	it = getFrequencyContour(x+noise,2048,1024,sf);
+	it(1) = 110;
+	it = log2(it);
+	logNoise(i) = 10*log10(i/100);
+	err(i) = immse(groundTruth,it);
+endfor
+
+plot(logNoise,log10(err));
+hold on;
+plot(logNoise,log10(ones(1,length(logNoise))*0.59*10^(-6)));
+grid;
+hold off;
+xlabel("Added White Noise (db)");
+ylabel("Mean Square Pitch Error (log10)");
+axis([min(logNoise) max(logNoise)]);
+
+set(gca,'fontsize', 12)
+set(gcf,'PaperUnits','centimeters','PaperPosition',[0 0 30 8])
+print -dpng 'NoiseRobustnessZCM.png'
---
+++ MetricTests/getFrequencyContour.m
@@ -0,0 +1,18 @@
+function contour = getFrequencyContour(sample,segmentSize,hopSize,sf)
+	% Pre-filter signal for ZCM: Assume: 20 < signal < 250Hz
+	[b,a] = butter(8, 250/sf*2);
+	sample = filter(b,a,sample);
+
+	% Segment data (Input should be exact length!!!)
+	sample = segment(sample,segmentSize,hopSize);
+
+	% Pre populate array
+	contour = ones(1,length(sample)/segmentSize)*220;
+
+	% Get Frequency of each segment
+	prev = 220;
+	for (i = 1:columns(sample))
+		contour(i) = getFrequencyZCM(sample(:,i),prev,sf);
+		prev = contour(i);
+	endfor
+endfunction
